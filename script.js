8b3e72b5853f4ec87985f5484619101324dba656
rochannn <147847413+rochannn@users.noreply.github.com>
2024-02-09 23:40:41 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,38 +1,9 @@
 // script.js
-var noClicks = 0;
-
-function moveNo() {
-    var noButton = document.getElementById("noButton");
-    var letterContainer = document.getElementById("letterContainer");
-    noClicks++;
-    if (noClicks >= 5) {
-        noButton.style.display = "none";
-    } else {
-        var xPos = Math.random() * (letterContainer.offsetWidth - noButton.offsetWidth);
-        var yPos = Math.random() * (letterContainer.offsetHeight - noButton.offsetHeight);
-        noButton.style.position = "absolute";
-        noButton.style.left = xPos + "px";
-        noButton.style.top = yPos + "px";
-    }
-}
-
-
-function expandYes() {
-    var yesButton = document.getElementById("yesButton");
-    var noButton = document.getElementById("noButton");
-    yesButton.textContent = "I LOVEEEEE YOUUUUU SOO MUCHHHH MOREEEEEE";
-    yesButton.style.backgroundColor = "#FFD1DA	";
-    yesButton.style.color = "black";
-    yesButton.style.fontWeight = "bold";
-    yesButton.style.fontSize = "20px"; 
-    noButton.style.display = "none";
-}
-
 document.getElementById("letterGif").addEventListener("click", function() {
     var letterContainer = document.getElementById("letterContainer");
     if (letterContainer.style.display === "none") {
         document.body.style.transition = "background-color 0.5s ease";
-        document.body.style.backgroundColor = "#FBA1B7";
+        document.body.style.backgroundColor = "#FFDDCC";
         letterContainer.style.display = "block";
         setTimeout(function() {
             letterContainer.style.top = "calc(100% - 20px)"; // Adjust as needed
@@ -40,7 +11,7 @@ document.getElementById("letterGif").addEventListener("click", function() {
         }, 400); // Delay for smoother transition
     } else {
         document.body.style.transition = "background-color 0.5s ease";
-        document.body.style.backgroundColor = "white";
+        document.body.style.backgroundColor = "#FFEECC";
         letterContainer.style.top = "100%";
         letterContainer.style.opacity = "0";
         setTimeout(function() {
